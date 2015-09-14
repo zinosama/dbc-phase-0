0d80a3851c6b1348b7628ecf340334b996c640f9
Ziåno Hu <rhu5@u.rochester.edu>
2015-09-13 20:38:03 -0400

    added week 4.5.1 and 4.5.2

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,22 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+def get_grade(input)
+	if input<=100 && input>=90
+		return "A"
+	elsif input<90 && input>=80
+		return "B"
+	elsif input<80 && input>=70
+		return "C"
+	elsif input<70 && input>=60
+		return "D"
+	elsif input<60
+		return "F"
+	end
+end
+		
+		
+		
